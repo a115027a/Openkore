d9a3d9ebbc2a4fe58a43fe7a5bea2ce6ef4c3d1d
許景然 <bear_hsu@softstar.com.tw>
2016-07-26 17:59:25 +0800

    2016.07.26 - TWRo 20160726 update data

--- src/Network/Send/twRO.pm
+++ src/Network/Send/twRO.pm
@@ -28,58 +28,63 @@ sub new {
 	$self->{char_create_version} = 1;
 
 	my %packets = (
-		'0369' => ['actor_action', 'a4 C', [qw(targetID type)]],
-		'0360' => ['buy_bulk_request', 'a4', [qw(ID)]],
-		'0868' => ['item_drop', 'v2', [qw(index amount)]],
-		'0872' => ['friend_request', 'a*', [qw(username)]],
-		'0957' => ['homunculus_command', 'v C', [qw(commandType, commandID)]],
-		'0880' => ['actor_look_at', 'v C', [qw(head body)]],
-		'0362' => ['map_login', 'a4 a4 a4 V C', [qw(accountID charID sessionID tick sex)]],
-		'0437' => ['character_move','a3', [qw(coords)]],
-		'0A68' => ['party_join_request_by_name', 'Z24', [qw(partyName)]],
-		'096A' => ['actor_info_request', 'a4', [qw(ID)]],
-		'02C4' => ['skill_use', 'v2 a4', [qw(lv skillID targetID)]],
-		'0438' => ['skill_use_location', 'v4', [qw(lv skillID x y)]],
-		'087E' => ['storage_item_add', 'v V', [qw(index amount)]],
-		'086E' => ['storage_item_remove', 'v V', [qw(index amount)]],
-		'094A' => ['storage_password'],
-		'035F' => ['sync', 'V', [qw(time)]],
-		'07EC' => ['item_take', 'a4', [qw(ID)]],
-		
-		'0368' => ['actor_name_request', 'a4', [qw(ID)]],
-		'0970' => ['char_create', 'a24 C v2', [qw(name, slot, hair_style, hair_color)]],
-		'07D7' => ['party_setting', 'V C2', [qw(exp itemPickup itemDivision)]],
-		'0801' => ['buy_bulk_vender', 'x2 a4 a4 a*', [qw(venderID venderCID itemInfo)]],
-		'0998' => ['send_equip', 'v V', [qw(index type)]],
-		'0064' => ['master_login', 'V Z24 a24 C', [qw(version username password_rijndael master_version)]]
+		'0931' => ['actor_action', 'a4 C', [qw(targetID type)]],
+		'0923' => ['skill_use', 'v2 a4', [qw(lv skillID targetID)]],
+		'0928' => ['character_move','a3', [qw(coords)]],
+		'0878' => ['sync', 'V', [qw(time)]],
+		'091E' => ['actor_look_at', 'v C', [qw(head body)]],
+		'0958' => ['item_take', 'a4', [qw(ID)]],
+		'0918' => ['item_drop', 'v2', [qw(index amount)]],
+		'088B' => ['storage_item_add', 'v V', [qw(index amount)]],
+		'0894' => ['storage_item_remove', 'v V', [qw(index amount)]],
+		'0959' => ['skill_use_location', 'v4', [qw(lv skillID x y)]],
+		#PACKET_CZ_USE_SKILL_TOGROUND_WITHTALKBOX2'0868'''''
+		'0202' => ['actor_info_request', 'a4', [qw(ID)]],
+		'0921' => ['actor_name_request', 'a4', [qw(ID)]],
+		#PACKET_CZ_SSILIST_ITEM_CLICK'0363'''''
+		#PACKET_CZ_SEARCH_STORE_INFO_NEXT_PAGE'0920'''''
+		#PACKET_CZ_SEARCH_STORE_INFO'08A0'''''
+		'092F' => ['buy_bulk_buyer', 'a4 a4 a*', [qw(buyerID buyingStoreID itemInfo)]],
+		'0949' => ['buy_bulk_request', 'a4', [qw(ID)]],
+		'0899' => ['buy_bulk_closeShop'],''
+		'0968' => ['buy_bulk_openShop', 'a4 c a*', [qw(limitZeny result itemInfo)]],
+		'0945' => ['booking_register', 'v8', [qw(level MapID job0 job1 job2 job3 job4 job5)]],
+		#PACKET_CZ_JOIN_BATTLE_FIELD'086B'''''
+		#PACKET_CZ_ITEMLISTWIN_RES'0893'''''
+		'0950' => ['map_login', 'a4 a4 a4 V C', [qw(accountID charID sessionID tick sex)]],
+		'083C' => ['party_join_request_by_name', 'Z24', [qw(partyName)]],
+		#PACKET_CZ_GANGSI_RANK'0935'''''
+		'087D' => ['friend_request', 'a*', [qw(username)]],
+		'0932' => ['homunculus_command', 'v C', [qw(commandType, commandID)]],
+		'0941' => ['storage_password']
 		);
 	$self->{packet_list}{$_} = $packets{$_} for keys %packets;
 
 	
 	my %handlers = qw(
-		actor_action 0369
-		buy_bulk_request 0360
-		item_drop 0868
-		friend_request 0872
-		homunculus_command 0957
-		actor_look_at 0880
-		map_login 0362
-		character_move 0437
-		party_join_request_by_name 0A68
-		actor_info_request 096A
-		skill_use 02C4
-		skill_use_location 0438
-		storage_item_add 087E
-		storage_item_remove 086E
-		storage_password 094A
-		sync 035F
-		item_take 07EC
-		
-		actor_name_request 0368
-		char_create 0970
-		party_setting 07D7
-		buy_bulk_vender 0801
-		send_equip 0998
+	
+		actor_action 0931
+		skill_use 0923
+		character_move 0928
+		sync 0878
+		actor_look_at 091E
+		item_take 0958
+		item_drop 0918
+		storage_item_add 088B
+		storage_item_remove 0894
+		skill_use_location 0959
+		actor_info_request 0202
+		actor_name_request 0921
+		buy_bulk_buyer 092F
+		buy_bulk_request 0949
+		buy_bulk_closeShop 0899
+		buy_bulk_openShop 0968
+		booking_register 0945
+		map_login 0950
+		party_join_request_by_name 083C
+		friend_request 087D
+		homunculus_command 0932
+		storage_password 0941
 	);
 
 	$self->{packet_lut}{$_} = $handlers{$_} for keys %handlers;
